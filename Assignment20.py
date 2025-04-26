bbba51b64786382ac404472b53f6c00b8becc9e6
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-27 00:07:44 +0530

    Add files via upload

---
+++ Assignment20.py
@@ -0,0 +1,30 @@
+"""
+#Q1
+for x in range(1,11):
+    print(x*5)
+
+
+#Q2
+n=int(input("Enter value of N "))
+for e in range(1,11):
+    print(e*n,end=' ')
+
+#Q3
+m=int(input("how many multiple of N u want to enter  "))
+n=int(input("Enter value of n "))
+for x in range(1,m+1):
+    print(x*n)
+
+    """
+
+
+#Q4
+n=int(input("Enter value of N "))
+for e in range(10,0,-1):
+    print(e*n)
+
+#Q5
+n=int(input("Enter a number to print its table "))
+for e in range(1,11):
+    print("%d x %d = %d"%(n,e,e*n))
+  
